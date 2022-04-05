2a88b8405594587509b14fbb22e18d41e43f3bb6
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2022-04-04 22:42:13 -0300

    make the create user area

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Tr
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import Pagination from '../../components/Form/Pagination'
 import { Header } from '../../components/Header'
 import { SideBar } from '../sidebar'
 
@@ -76,6 +77,7 @@ export default function UsersList() {
               </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
